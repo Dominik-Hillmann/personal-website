6ad2872c849b721da5b05b9ad51fdc8d1b3d5319
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-02-05 18:43:27 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+
+<!-- Basic Page Needs -->
+    <meta charset="utf-8">
+    <title>Missing Title</title>
+    <meta name="description" content=""> <!-- DO NOT FORGET DESCRIPTION-->
+    <meta name="author" content="Dominik Hillmann">
+
+<!-- Mobile Specific Metas -->
+    <meta>
+
+<!-- CSS links -->
+    <link rel="stylesheet" href="css/main.css">
+
+<!-- Favicon -->
+    <link rel="icon" type="image/png" href="">
+
+</head>
+<body>
+
+<!-- Page Layout -->
+
+   <header>
+   <!-- Teile: "Ganz oben mit p5-Sketch", Kontakt, Skills, Resumé, brief history [reading, ]-->
+      <div><img src="images/hollow_circle.png"><a href="#">START</a></div>
+      <div><img src="images/hollow_circle.png"><a href="#">SKILLS</a></div>
+      <div><img src="images/hollow_circle.png"><a href="#">KONTAKT</a></div>  
+   </header>
+
+
+
+<!-- End of Page Layout -->
+</body>
+</html>
+
+<!-- Links to Scripts -->
