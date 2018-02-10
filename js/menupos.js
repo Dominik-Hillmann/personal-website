1832f559b2f309a1162a923c3fcba72c0ba9cfae
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-02-10 21:49:50 +0100

    Delete menupos.js

--- js/menupos.js
+++
@@ -1,9 +0,0 @@
-var menu = document.getElementById("menuwrapper");
-
-function adaptmenu()
-{
-   menu.style.marginTop = ((window.innerHeight / 2) - menu.offsetHeight / 2) + "px";
-}
-
-adaptmenu(); // adapting for the first time after loading
-window.addEventListener("resize", adaptmenu); // every time the window size is changed
