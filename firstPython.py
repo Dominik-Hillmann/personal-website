060a13605bd62c732213e9ac264dfa2a84eea018
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-09-02 22:47:32 +0200

    Delete firstPython.py

--- firstPython.py
+++
@@ -1,5 +0,0 @@
-# Write some code that creates a list of integers, loops through each element of the list, and only prints out even numbers!
-intList = range(10)
-for ele in intList:
-    if ele % 2 == 0:
-        print(ele),
