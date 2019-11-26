046a86bd2d7256e506e5d44001c6239be5756743
Dominik-Hillmann <dominik.hillmann@gmx.de>
2019-11-26 21:47:04 +0100

    Finished basic structure

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <!-- Basic Page Needs -->
         <meta charset="utf-8">
-        <title>BRAINRAIN</title>
+        <title>Dominik Hillmann</title>
         <meta name="description" content="Personal website of Dominik Hillmann.">
         <meta name="author" content="Dominik Hillmann">
 
@@ -18,6 +18,9 @@
         <!-- Favicon -->
         <!-- <link rel="icon" type="image/png" href="favicon.ico"> -->
     </head>
+
+    <div id="logo"><h1>Dominik Hillmann</h1></div>
+
     <body>
         <!-- Sticky side menu -->
         <div id="sidemenu">
@@ -47,8 +50,8 @@
                 <!-- Waves: https://getwaves.io/ -->
                 <div class="post" id="welcomepost">
                     <div class="postheader">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,288L40,272C80,256,160,224,240,208C320,192,400,192,480,176C560,160,640,128,720,96C800,64,880,32,960,58.7C1040,85,1120,171,1200,213.3C1280,256,1360,256,1400,256L1440,256L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
                         <h2>Das ist ein Test in der Überschrift.</h2>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,288L40,272C80,256,160,224,240,208C320,192,400,192,480,176C560,160,640,128,720,96C800,64,880,32,960,58.7C1040,85,1120,171,1200,213.3C1280,256,1360,256,1400,256L1440,256L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
                     </div>
 
                     <p>
@@ -58,8 +61,8 @@
 
                 <div class="post">
                     <div class="postheader">
+                        <h2>Das ist ein Test in der Überschrift.22222222</h2>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,192L40,186.7C80,181,160,171,240,181.3C320,192,400,224,480,234.7C560,245,640,235,720,202.7C800,171,880,117,960,101.3C1040,85,1120,107,1200,144C1280,181,1360,235,1400,261.3L1440,288L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
-                        <h2>Das ist ein Test in der Überschrift.</h2>
                     </div>
 
                     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
@@ -67,8 +70,8 @@
                 
                 <div class="post">
                     <div class="postheader">
+                        <h2>Das ist ein Test in der Überschrift33333333.</h2>
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,320L40,320C80,320,160,320,240,293.3C320,267,400,213,480,176C560,139,640,117,720,133.3C800,149,880,203,960,218.7C1040,235,1120,213,1200,181.3C1280,149,1360,107,1400,85.3L1440,64L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
-                        <h2>Das ist ein Test in der Überschrift.</h2>
                     </div>
 
                     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
